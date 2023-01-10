5884a4bd8eae26eb83787ac9bba9d74b16916bc6
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-01-10 17:02:02 -0300

    Created first migration

---
+++ prisma/migrations/20230110200055_create_user_model/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "address" TEXT,
+    "password" TEXT NOT NULL,
+    "created_at" DATETIME DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME DEFAULT CURRENT_TIMESTAMP
+);
