185f7731e8331d28d3c63986f1293812a1db5ab6
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-01-10 18:47:11 -0300

    Created CreateUserService initial class

---
+++ src/services/user/CreateUserService.ts
@@ -0,0 +1,7 @@
+import prismaCLient from "../../prisma";
+import { hash } from "bcryptjs";
+import { UserRequest } from '../../models/interfaces/UserRequest';
+
+class CreateUserService { }
+
+export { CreateUserService };
