0380c4a3e95ee161f430561dc521b3bf6977cbf9
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-01-11 18:49:57 -0300

    Added category routes

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,8 @@ import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
 
 const router = Router();
 
@@ -10,5 +12,9 @@ const router = Router();
 router.post("/user", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
 router.get("/me", isAuthenticated, new DetailUserController().handle);
+
+// Category Routes
+router.post("/category", isAuthenticated, new CreateCategoryController().handle);
+router.get("/category", isAuthenticated, new ListCategoryController().handle)
 
 export { router };
