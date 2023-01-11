6122027e598065d79d7a1e9aeeb8bbc839b645af
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-01-11 17:57:38 -0300

    Added CreatedCategoryService

---
+++ src/services/category/CreateCategoryService.ts
@@ -0,0 +1,21 @@
+import prismaCLient from "../../prisma";
+
+class CreateCategoryService {
+    async execute({ name }: CategoryRequest) {
+        if (name === "" || name === null || !name) {
+            throw new Error("Invalid name");
+        }
+        const category = await prismaCLient.category.create({
+            data: {
+                name: name,
+            },
+            select: {
+                id: true,
+                name: true,
+            },
+        });
+        return category;
+    }
+}
+
+export { CreateCategoryService };
