393742e650e1d8e7f83183016dbdb0935ea74285
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-01-11 16:16:47 -0300

    Created CreateUserController

--- src/controllers/user/CreateUserController.ts
+++ src/controllers/user/CreateUserController.ts
@@ -1,16 +1,18 @@
 import { Request, Response } from "express";
 import { CreateUserService } from "../../services/user/CreateUserService";
-import { UserRequest } from '../../models/interfaces/UserRequest';
+import { UserRequest } from "../../models/interfaces/UserRequest";
 
 class CreateUserController {
-    async handle(req: Request, res: Response) {
-        const { name, email, password }: UserRequest = req?.body;
-        const createUserService = new CreateUserService();
-        const user = await createUserService.execute({
-            name, email, password
-        });
-        return res.json(user);
-    }
+  async handle(req: Request, res: Response) {
+    const { name, email, password }: UserRequest = req?.body;
+    const createUserService = new CreateUserService();
+    const user = await createUserService.execute({
+      name,
+      email,
+      password,
+    });
+    return res.json(user);
+  }
 }
 
 export { CreateUserController };
