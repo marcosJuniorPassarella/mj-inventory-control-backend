850acd55aa74e0926cc8724821ce7d58f2e4cae4
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-01-11 18:01:42 -0300

    Added listCategoryService

---
+++ src/services/category/ListCategoryService.ts
@@ -0,0 +1,15 @@
+import prismaCLient from "../../prisma";
+
+class ListCategoryService {
+  async execute() {
+    const categories = await prismaCLient.category.findMany({
+      select: {
+        id: true,
+        name: true,
+      },
+    });
+    return categories;
+  }
+}
+
+export { ListCategoryService };
