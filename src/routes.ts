da60b56c71a65c6ad737634afb05c81097aeb797
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-01-11 17:46:55 -0300

    Added new updates

--- src/routes.ts
+++ src/routes.ts
@@ -7,8 +7,8 @@ import { isAuthenticated } from "./middlewares/isAuthenticated";
 const router = Router();
 
 // User Routes
-router.post("/users", new CreateUserController().handle);
+router.post("/user", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
-router.post("/me", isAuthenticated, new DetailUserController().handle);
+router.get("/me", isAuthenticated, new DetailUserController().handle);
 
 export { router };
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 import cors from "cors";
+import path from "path";
 import { router } from "./routes";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(router);
+app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     if (err instanceof Error) {
         return res.status(400).json({
@@ -19,6 +21,4 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     });
 });
 
-app.listen(3333, () =>
-    console.log("Servidor rodando na porta 3333 - Inventory Control")
-);
+app.listen(3333, () => console.log("Servidor rodando na porta 3333"));
