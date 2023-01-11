600fb44563ee2a15346d37060f363860348eca32
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-01-11 16:17:05 -0300

    Created AuthUserController

---
+++ src/controllers/user/AuthUserController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { AuthUserService } from "../../services/user/AuthUserService";
+import { AuthRequest } from "../../models/interfaces/AuthRequest";
+
+class AuthUserController {
+  async handle(req: Request, res: Response) {
+    const { email, password }: AuthRequest = req.body;
+    const authUserService = new AuthUserService();
+    const auth = await authUserService.execute({
+      email,
+      password,
+    });
+    return res.json(auth);
+  }
+}
+
+export { AuthUserController };
