435c105bed25502eba59f1a085ba774cf1205d71
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-01-11 16:33:37 -0300

    Added new routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,14 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
+import { CreateUserController } from "./controllers/user/CreateUserController";
+import { AuthUserController } from "./controllers/user/AuthUserController";
+import { DetailUserController } from "./controllers/user/DetailUserController";
+import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router();
 
-router.get("/test", (req: Request, res: Response) => {
-    return res.json({ ok: true });
-});
+// User Routes
+router.post("/users", new CreateUserController().handle);
+router.post("/session", new AuthUserController().handle);
+router.post("/me", isAuthenticated, new DetailUserController().handle);
 
 export { router };
