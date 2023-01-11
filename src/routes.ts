0512493d4510e3e3b2c79dd5026e5bbcfffad6b5
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-01-11 19:58:20 -0300

    Added multer config

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,17 @@
 import { Router } from "express";
+import multer from "multer";
+import uploadConfig from "./config/multer";
+import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
-import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
 import { RemoveCategoryController } from "./controllers/category/RemoveCategoryController";
+import { CreateProductController } from "./controllers/product/CreateProductController";
 
 const router = Router();
+const upload = multer(uploadConfig.upload("./tmp"));
 
 // User Routes
 router.post("/user", new CreateUserController().handle);
@@ -15,8 +19,24 @@ router.post("/session", new AuthUserController().handle);
 router.get("/me", isAuthenticated, new DetailUserController().handle);
 
 // Category Routes
-router.post("/category", isAuthenticated, new CreateCategoryController().handle);
+router.post(
+  "/category",
+  isAuthenticated,
+  new CreateCategoryController().handle
+);
 router.get("/category", isAuthenticated, new ListCategoryController().handle);
-router.delete("/category/remove", isAuthenticated, new RemoveCategoryController().handle);
+router.delete(
+  "/category/remove",
+  isAuthenticated,
+  new RemoveCategoryController().handle
+);
+
+// Product Routes
+router.post(
+  "/product",
+  isAuthenticated,
+  upload.single("file"),
+  new CreateProductController().handle
+);
 
 export { router };
