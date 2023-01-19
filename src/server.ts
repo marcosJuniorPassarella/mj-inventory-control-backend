77c0ee9461afb05546b6ede21cbc959d1943138b
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-01-19 17:23:32 -0300

    Added initial implementation of swagger

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,16 @@ import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 import cors from "cors";
 import path from "path";
+import swaggerUi from "swagger-ui-express";
 import { router } from "./routes";
+import swaggerDocs from "./swagger.json";
 
+const port = 3333;
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(router);
+app.use("/v1", router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     if (err instanceof Error) {
@@ -21,4 +25,9 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     });
 });
 
-app.listen(3333, () => console.log("Servidor rodando na porta 3333"));
+app.get('/terms', (request: Request, response: Response) => {
+    return response.json({
+        message: 'Termos de Serviço'
+    })
+})
+app.listen(port, () => console.log("Servidor rodando na porta 3333"));
