6c058621a981dcc429007cafed98940f3b4690d9
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-01-21 20:10:54 -0300

    Added first documentation with Swagger

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,14 @@ import cors from "cors";
 import path from "path";
 import swaggerUi from "swagger-ui-express";
 import { router } from "./routes";
-import swaggerDocs from "./swagger.json";
+import swaggerDocument from "../swagger.json";
 
 const port = 3333;
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/v1", router);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     if (err instanceof Error) {
